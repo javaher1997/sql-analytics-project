2f9f8f5bc21aa3e81bab8a9beb842e19284bc84c
Javaher <javahernourian@gmail.com>
2026-02-05 11:56:55 +0330

    add description

---
+++ sql/insert_data.sql
@@ -0,0 +1,10 @@
+/*
+Project: SQLite E-commerce Database
+Author: Javaher Nourian
+Description:
+Populates the database with sample data for testing and analysis.
+Includes customers, products, orders, and order items.
+
+Usage:
+Run after schema.sql
+*/
