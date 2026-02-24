b70709c9ef307ebd4835252f8c9348b131ede2f2
Javaher <javahernourian@gmail.com>
2026-02-24 18:17:48 +0330

    provide list of products with certain columns

---
+++ sql/queries/basic/list_products.sql
@@ -0,0 +1,12 @@
+/*
+Query: List all products
+Description: Retrieves product_id, product name, category, price
+*/
+
+SELECT
+    product_id,
+    product_name,
+    category,
+    price
+FROM
+    products
